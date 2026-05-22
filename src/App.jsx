e3d296cdc49f034fc5b777644ac48021dda6227d
Andre Maciel <andreefc23@gmail.com>
2026-05-21 17:25:37 -0700

    feat(red box): Mesh template implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { UI } from "./components/UI";
+import { Experience } from "./components/Experience";
 
 function App() {
     return (
         <>
             <UI />
+            <Canvas
+                shadows
+                camera={{ position: [-0.5, 1, 4], fov: 45 }}
+            >
+                <Suspense fallback={null}>
+                    <Experience />
+                </Suspense>
+            </Canvas>
             <Loader />
         </>
     );
